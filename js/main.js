e14b13c952c1716e750982cca68013fc75fe50aa
a.kudriavtsev <a.kudriavtsev@it-online.ru>
2013-05-10 20:21:33 +0400

    добавлен контент форма

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 $('a[data-toggle=popover]').popover({trigger: 'hover focus'});
+$('span[data-toggle=popover]').popover({trigger: 'hover focus'});
 $('#region').typeahead({
 	'items':3,
 	'source':["Москва","Москва и область","Самара","Санкт-Петербург","Саратов","Alabama","Alaska","Arizona","Arkansas","California","Colorado","Connecticut","Delaware","Florida","Georgia","Hawaii","Idaho","Illinois","Indiana","Iowa","Kansas","Kentucky","Louisiana","Maine","Maryland","Massachusetts","Michigan","Minnesota","Mississippi","Missouri","Montana","Nebraska","Nevada","New Hampshire","New Jersey","New Mexico","New York","North Dakota","North Carolina","Ohio","Oklahoma","Oregon","Pennsylvania","Rhode Island","South Carolina","South Dakota","Tennessee","Texas","Utah","Vermont","Virginia","Washington","West Virginia","Wisconsin","Wyoming"]
